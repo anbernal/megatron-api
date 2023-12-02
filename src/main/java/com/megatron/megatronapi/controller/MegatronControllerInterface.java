9e2f6168b60d29801a6fd431d4abdfac6658d492
zarpsystem_asantos <zarpsystem_asantos@orizon.com.br>
2023-12-02 16:50:02 -0300

    mais ajustes para conexao do banco

--- src/main/java/com/megatron/megatronapi/controller/MegatronControllerInterface.java
+++ src/main/java/com/megatron/megatronapi/controller/MegatronControllerInterface.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api") // endpoint raiz (pai)
+@RequestMapping("/api")
 public interface MegatronControllerInterface {
 
     @GetMapping("/teste")
